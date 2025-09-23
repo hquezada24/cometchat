6ff7a4132125a60fc4e961ced5242aa48b070578
Hugo <hquezada@leomail.tamuc.edu>
2025-09-23 16:53:04 -0500

    feat: create listen function

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 // Global Middleware
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server running on localhost:${process.env.PORT ? process.env.PORT : 3000}`
+  );
+});
