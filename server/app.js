d3f70508f9f46243f6206681227fc15334f4204c
Hugo <hquezada@leomail.tamuc.edu>
2025-09-24 11:06:06 -0500

    refactor: move app.js to server folder

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const createAccountRoutes = require("./src/routes/createAccountRoutes");
+const cors = require("cors");
+require("dotenv").config();
+
+// Global Middleware
+app.use(express.json());
+
+const corsOptions = {
+  origin: "http://localhost:5173", // or whatever your React app's port is
+  optionsSuccessStatus: 200, // For legacy browser support
+};
+
+// Use the cors middleware
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/", createAccountRoutes);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `Server running on localhost:${process.env.PORT ? process.env.PORT : 3000}`
+  );
+});
