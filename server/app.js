967b487ddcebef77e78b6176760ef50628d805b9
Hugo <hquezada@leomail.tamuc.edu>
2025-09-25 15:19:22 -0500

    refactor: add /api/me get route

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const createAccountRoutes = require("./src/routes/createAccountRoutes");
 const loginRoutes = require("./src/routes/loginRoutes");
+const { authStatusController } = require("./src/controllers/authController");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -11,11 +12,16 @@ app.use(express.json());
 const corsOptions = {
   origin: "http://localhost:5173", // or whatever your React app's port is
   optionsSuccessStatus: 200, // For legacy browser support
+  credentials: true,
 };
 
 // Use the cors middleware
 app.use(cors(corsOptions));
 
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.get("/api/me", authStatusController);
 app.get("/", (req, res) => {
   res.send("hello");
 });
