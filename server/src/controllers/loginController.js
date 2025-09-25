d78b5e503003c11c2c4e02324f492ffd942b0ba4
Hugo <hquezada@leomail.tamuc.edu>
2025-09-25 15:18:25 -0500

    refactor: make sameSite property 'none'

--- server/src/controllers/loginController.js
+++ server/src/controllers/loginController.js
@@ -30,7 +30,7 @@ const loginController = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 60 * 60 * 1000, // 1 hour
     });
     res.json({
