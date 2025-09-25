ba4bf7c034f713b324280c2358fa615a8a1136f0
Hugo <hquezada@leomail.tamuc.edu>
2025-09-25 13:56:07 -0500

    refactor: add Protected route. Add Home and AuthLayout to App children array

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,4 +1,7 @@
+// routes.tsx
 import App from "./App";
+import { ProtectedRoute } from "./components/ProtectedRoute";
+import Home from "./components/Home/Home";
 import AuthLayout from "./components/AuthLayout/AuthLayout";
 import CreateAccount from "./components/CreateAccount/CreateAccount";
 import Login from "./components/Login/Login";
@@ -6,13 +9,23 @@ import Login from "./components/Login/Login";
 const routes = [
   {
     path: "/",
-    element: <App />,
-  },
-  {
-    element: <AuthLayout />,
+    element: <App />, // Root element with AuthProvider
     children: [
-      { path: "/login", element: <Login /> },
-      { path: "/create-account", element: <CreateAccount /> },
+      {
+        path: "",
+        element: (
+          <ProtectedRoute>
+            <Home />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        element: <AuthLayout />,
+        children: [
+          { path: "login", element: <Login /> },
+          { path: "create-account", element: <CreateAccount /> },
+        ],
+      },
     ],
   },
 ];
