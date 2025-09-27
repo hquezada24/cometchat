bdb121bd8449b99250e64a51ad0c394d31b0a7ba
Hugo <hquezada@leomail.tamuc.edu>
2025-09-27 10:26:02 -0500

    refactor: import isLoading state from provider. Add Loading component when isLoading is true.

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -7,7 +7,11 @@ export const ProtectedRoute = ({
 }: {
   children: React.JSX.Element;
 }) => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
+
+  if (isLoading) {
+    return <div>Loading...</div>; // Or your loading component
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
