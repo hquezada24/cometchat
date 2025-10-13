e96b319aafa69673290e1fed936f2a2f9d724f74
Hugo <hquezada@leomail.tamuc.edu>
2025-10-13 11:44:39 -0500

    feat: create checkEmail function to send a get request to /api/check-email

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,8 +1,6 @@
 // src/services/userService.js
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
-
-import { method } from "lodash";
 import { apiRequest } from "./apiClient";
 
 function updateProfile(updates) {
@@ -21,5 +19,13 @@ const checkUsername = (username) => {
     }
   );
 };
+const checkEmail = (email) => {
+  return apiRequest(
+    `${API_BASE_URL}/api/check-email/${encodeURIComponent(email)}`,
+    {
+      credentials: "include",
+    }
+  );
+};
 
-export { updateProfile, checkUsername };
+export { updateProfile, checkUsername, checkEmail };
