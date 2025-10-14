cc4b94e756d541c7fd39b04fc0386676906e4e73
Hugo <hquezada@leomail.tamuc.edu>
2025-10-14 18:19:57 -0500

    feat: add ThemeProvider to App component for global theming

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
+import { ThemeProvider } from "./context/ThemeProvider";
 
 function App() {
   return (
     <div className="app-container">
       <AuthProvider>
-        <Outlet />
+        <ThemeProvider>
+          <Outlet />
+        </ThemeProvider>
       </AuthProvider>
     </div>
   );
