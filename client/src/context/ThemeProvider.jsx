1bf17f5674cefc7000ae60dd4b3e8f3ae6b665d5
Hugo <hquezada@leomail.tamuc.edu>
2025-10-14 18:15:41 -0500

    feat: create ThemeProvider

---
+++ client/src/context/ThemeProvider.jsx
@@ -0,0 +1,53 @@
+import { createContext, useEffect, useState } from "react";
+import { fetchTheme, updateTheme } from "../services/themeServices";
+import useAuth from "../hooks/useAuth";
+export const ThemeContext = createContext(null);
+
+export const ThemeProvider = ({ children }) => {
+  const isAuthenticated = useAuth();
+  const [theme, setTheme] = useState("light");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!isAuthenticated) return;
+
+    console.log("This is logged if authenticated");
+    const getTheme = async () => {
+      try {
+        const res = await fetchTheme();
+        if (!res) throw new Error("Failed to fetch theme");
+        const data = await res.json();
+        console.log("getTheme got: ", data);
+        setTheme(data.theme || "light");
+      } catch (error) {
+        console.error("Error fetching theme:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getTheme();
+  }, []);
+
+  const switchTheme = async (newTheme) => {
+    setTheme(newTheme);
+
+    try {
+      await updateTheme(newTheme);
+    } catch (error) {
+      console.error("Error updating theme:", error);
+    }
+  };
+
+  const toggleTheme = () => {
+    switchTheme(theme === "light" ? "dark" : "light");
+  };
+
+  if (loading) return null;
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
