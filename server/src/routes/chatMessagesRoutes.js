d183127b06661be8a6a3281016271ab2d0d245db
Hugo <hquezada@leomail.tamuc.edu>
2025-10-20 17:00:06 -0500

    feat: create api/chatrooms/:chatId/messages patch route with editMessage controller to update message content

--- server/src/routes/chatMessagesRoutes.js
+++ server/src/routes/chatMessagesRoutes.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
-const { sendMessage } = require("../controllers/chatMessagesController");
+const {
+  sendMessage,
+  editMessage,
+} = require("../controllers/chatMessagesController");
 const authenticate = require("../middleware/authMiddleware");
 
 const router = Router();
 
 // Send a message to a chatroom
 router.post("/:chatId/messages", authenticate, sendMessage);
+router.patch("/:chatId/messages", authenticate, editMessage);
 
 // Get all messages from a chatroom
 // router.get("/:chatId/messages", getChatroomMessages);
