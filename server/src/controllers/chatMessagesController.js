62a2f07356abd67ec8098fd3855f914f28828ce8
Hugo <hquezada@leomail.tamuc.edu>
2025-10-21 16:59:21 -0500

    feat: create deleteMessage controller for message deletion

--- server/src/controllers/chatMessagesController.js
+++ server/src/controllers/chatMessagesController.js
@@ -1,10 +1,17 @@
-const { createMessage, modifyMessage } = require("../models/messages");
+const {
+  createMessage,
+  modifyMessage,
+  removeMessage,
+} = require("../models/messages");
 
 const sendMessage = async (req, res) => {
   try {
     const { chatId } = req.params;
     const { message } = req.body;
     const userId = req.user?.id;
+
+    console.log(message);
+    console.log(userId);
 
     if (!userId) {
       return res.status(401).json({ error: "Unauthorized" });
@@ -46,4 +53,31 @@ const editMessage = async (req, res) => {
   }
 };
 
-module.exports = { sendMessage, editMessage };
+const deleteMessage = async (req, res) => {
+  try {
+    const { chatId } = req.params;
+    const { messageId } = req.body;
+    const userId = req.user?.id;
+
+    if (!userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    if (!messageId) {
+      return res.status(400).json({ error: "No Message id was provided" });
+    }
+
+    const result = await removeMessage(chatId, userId, messageId);
+
+    if (!result.success) {
+      return res.status(404).json(result);
+    }
+
+    return res.status(204).send();
+  } catch (error) {
+    console.error("Error modifying message:", error);
+    return res.status(500).json({ error: "Failed to modify message" });
+  }
+};
+
+module.exports = { sendMessage, editMessage, deleteMessage };
