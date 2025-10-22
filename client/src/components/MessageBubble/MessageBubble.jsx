e8f1d78f6b90e60704fd3d9b3a838b49a16b4b34
Hugo <hquezada@leomail.tamuc.edu>
2025-10-22 13:32:25 -0500

    feat: create handleEditSubmit function. Create handleDelete function. Create handleReset function. Create editMessage state to edit messages by turning the message into a textarea input box. Create useEffect to make textarea focus by default when editing.

--- client/src/components/MessageBubble/MessageBubble.jsx
+++ client/src/components/MessageBubble/MessageBubble.jsx
@@ -1,26 +1,67 @@
 import "./MessageBubble.css";
 import useTheme from "../../hooks/useTheme";
 import Navigation from "../Navigation/Navigation";
-import { ChevronDown } from "lucide-react";
+import { modifyMessage, deleteMessage } from "../../services/apiServices";
+import { ChevronDown, Check, X } from "lucide-react";
 import { useLocation } from "react-router-dom";
-
-const navigationItems = [{ label: "Edit" }, { label: "Delete" }];
+import { useState, useRef, useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
 
 const MessageBubble = ({
   messageRow,
   messageBubble,
   onClick,
   isMenuOpen,
-  navClassName,
   message,
+  messageId,
   date,
+  onDeleteLocal,
 }) => {
   const { theme } = useTheme();
-  const location = useLocation;
+  const [editMessage, setEditMessage] = useState(false);
+  const [messageValue, setMessageValue] = useState(message);
+  const [msgId, setMessageId] = useState(messageId);
+  const location = useLocation();
+  const { selectedChat } = useAuth();
+  const textareaRef = useRef(null);
 
+  const navigationItems = [
+    { label: "Edit", onClick: () => setEditMessage(true) },
+    { label: "Delete", onClick: () => handleDelete() },
+  ];
   const isActiveLink = (path) => {
     return location.pathname === path;
   };
+
+  const handleEditSubmit = async () => {
+    try {
+      await modifyMessage(selectedChat.id, messageValue, msgId);
+    } catch (error) {
+      console.error("Failed to modify message: ", error);
+    }
+  };
+  const handleDelete = async () => {
+    try {
+      await deleteMessage(selectedChat.id, msgId);
+
+      onDeleteLocal(msgId);
+    } catch (error) {
+      console.error("Failed to delete message: ", error);
+    }
+  };
+
+  const handleReset = async () => {
+    setMessageValue(message);
+    setEditMessage(false);
+  };
+
+  useEffect(() => {
+    if (editMessage && textareaRef.current) {
+      const el = textareaRef.current;
+      el.focus();
+      el.selectionStart = el.selectionEnd = el.value.length;
+    }
+  }, [editMessage]);
 
   return (
     <div className={`message-row ${messageRow}`}>
@@ -28,24 +69,53 @@ const MessageBubble = ({
         className={`message-bubble ${messageBubble}`}
         data-theme={theme === "dark" ? "dark" : ""}
       >
-        <div className="nav-container">
-          <div className="button-container">
-            <button
-              className="options slide-button"
-              data-theme={theme === "dark" ? "dark" : ""}
-              onClick={onClick}
-            >
-              <ChevronDown size={30} />
-            </button>
+        {editMessage ? null : (
+          <div className="nav-container">
+            <div className="button-container">
+              <button
+                className="options slide-button"
+                data-theme={theme === "dark" ? "dark" : ""}
+                onClick={onClick}
+              >
+                <ChevronDown size={30} />
+              </button>
+            </div>
+            <Navigation
+              navigationItems={navigationItems}
+              isMenuOpen={isMenuOpen}
+              isActiveLink={isActiveLink}
+            />
           </div>
-          <Navigation
-            navigationItems={navigationItems}
-            isMenuOpen={isMenuOpen}
-            isActiveLink={isActiveLink}
-            className={`${navClassName}`}
-          />
-        </div>
-        <p className="message-text">{message}</p>
+        )}
+        {editMessage ? (
+          <form className="edit-message-form" onSubmit={handleEditSubmit}>
+            <textarea
+              ref={textareaRef}
+              id="message-textarea"
+              name="message"
+              rows={`${Math.min(10, Math.ceil(messageValue.length / 50))}`}
+              cols="50"
+              wrap="soft"
+              data-theme={theme === "dark" ? "dark" : ""}
+              value={messageValue}
+              onChange={(e) => {
+                if (e.target.value.length <= 1000)
+                  setMessageValue(e.target.value);
+              }}
+            ></textarea>
+
+            <div className="edit-message-buttons">
+              <button type="submit" className="done">
+                <Check size={15} />
+              </button>
+              <button className="done" onClick={handleReset}>
+                <X size={15} />
+              </button>
+            </div>
+          </form>
+        ) : (
+          <p className="message-text">{message}</p>
+        )}
         <p className="message-time">
           {new Date(date).toLocaleTimeString([], {
             hour: "2-digit",
