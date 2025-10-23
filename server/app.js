1f1f0056ef54ece4bac36862479fe3e6a712ab82
Hugo <hquezada@leomail.tamuc.edu>
2025-10-23 16:09:44 -0500

    refactor: simplify corsOptions

--- server/app.js
+++ server/app.js
@@ -18,19 +18,8 @@ require("dotenv").config();
 // Global Middleware
 app.use(express.json());
 
-const allowedOrigins = [
-  "https://hqcometchat.netlify.app",
-  "http://localhost:5173",
-];
-
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://hqcometchat.netlify.app",
   optionsSuccessStatus: 200,
 };
 
